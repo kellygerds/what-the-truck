6c8dbcba736f60db5cecdb2b13118c292a0509e0
kelly <gerding4360@students.philau.edu>
2017-04-29 11:05:44 -0400

    Initial Commit

---
+++ CUSTOMER/scripts/script.js
@@ -0,0 +1,54 @@
+//hamburger menu//
+jQuery(document).ready(function($) {
+  $('.mobile-nav-toggle').on('click', function(e){
+      e.preventDefault();
+      $('.mobile-nav-wrapper').toggleClass('active');
+
+  });
+
+  $('.mobile-nav-exit').on('click', function(e){
+    e.preventDefault();
+    $('.mobile-nav-wrapper').removeClass('active');
+  })
+
+});
+
+var main = function() {
+
+  /* Ideas for additions:
+    * Allow user to select a post
+    * Add an "x" box to remove a post 
+  */
+
+  // Post the contents of status box to the page  
+  $('.btn').click(function() {
+    var post = $('.status-box').val();
+
+    $('<li>').text(post).prependTo('.posts');
+    $('.status-box').val('');
+    $('.counter').text("140");
+    $('.btn').addClass("disabled");
+  });
+
+  // Update the charCount counter
+  $('.status-box').keyup(function() {
+    var postLength = $(this).val().length;
+    var charactersLeft = 140 - postLength;
+
+    // Disable posting of text if charCount > 140 || charCount < 0
+    $('.counter').text(charactersLeft);
+
+    if (charactersLeft < 0) {
+      $('.btn').addClass("disabled");
+    } else if (charactersLeft > 140) {
+      $('.btn').addClass("disabled");
+    } else {
+      $('.btn').removeClass("disabled");
+    }
+  });
+
+  $('.btn').addClass("disabled");
+}
+
+$(document).ready(main);
+
